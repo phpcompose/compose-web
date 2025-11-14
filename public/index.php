577906a8abc6fcf7027646f4c40610e476a5fb2a
Alamin Ahmed <al.ahmed@outlook.com>
2025-11-13 21:27:43 -0500

    Initial setup

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+use Compose\Web\Config;
+use Compose\Starter;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$config = array_replace_recursive((new Config())(), [
+    'app' => ['name' => 'Compose Web'],
+    'template' => ['dir' => __DIR__ . '/../templates'],
+    'pages' => ['dir' => __DIR__ . '/../pages'],
+]);
+
+Starter::start($config);
---
+++ src/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Compose\Web;
+
+use Compose\Config as BaseConfig;
+
+/**
+ * Extension point for Compose configuration; currently behaves like the core config.
+ */
+final class Config extends BaseConfig
+{
+}
